c3dee09f16c15d90222de20d8df4df788de88ac4
AliBaig102 <baiga8424@gmail.com>
2024-06-15 01:54:21 +0500

    Introduction to JavaScript

---
+++ class01.js
@@ -0,0 +1,46 @@
+// - Introduction to JavaScript
+// - What is JavaScript?
+// - History and Evolution
+// - JavaScript in the Browser
+// - JavaScript on the Server (Node.js)
+// - Setting Up Your Development Environment
+
+// What is JavaScript?
+// - JavaScript is a programming language
+// - JavaScript is a high-level, interpreted language
+// - JavaScript is a scripting language
+// - JavaScript is a server-side scripting language
+// - JavaScript is a client-side scripting language
+
+// what is programming language?
+// - A programming language is a formal language that is used to create programs for computers.
+
+// What is a high-level, interpreted language?
+// - A high-level, interpreted language is a programming language that is designed to be easier to read and write.
+
+// What is a scripting language?
+// - A scripting language is a programming language that is designed to be used in a web browser.
+
+// What is a server-side scripting language?
+// - A server-side scripting language is a programming language that is designed to be used in a web server.
+
+// What is a client-side scripting language?
+// - A client-side scripting language is a programming language that is designed to be used in a web browser.
+
+// History and Evolution
+
+// JavaScript was created by Brendan Eich in 1995.
+// JavaScript was initially developed as a client-side scripting language in Netscape 2.0.
+// JavaScript was later developed as a server-side scripting language in Netscape 4.0.
+
+// Setting Up Your Development Environment
+// - Node.js
+// - Visual Studio Code
+
+// Node.js
+// - Node.js is a JavaScript runtime environment.
+// - Node.js is a server-side JavaScript runtime environment.
+
+// Visual Studio Code
+// - Visual Studio Code is a code editor.
+// - Visual Studio Code is a text editor.
