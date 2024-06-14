02fea96e89e42cb9ef77b76d539f2ff9ead694db
AliBaig102 <baiga8424@gmail.com>
2024-06-15 02:05:15 +0500

    Browser Object Model (BOM)

---
+++ class13.js
@@ -0,0 +1,56 @@
+// ### Browser Object Model (BOM)
+// - Window Object
+// - Navigator Object
+// - Screen Object
+// - Location Object
+// - History Object
+
+// Window Object
+// - window.alert
+// - window.confirm
+// - window.prompt
+// - window.setTimeout
+// - window.setInterval
+// - window.clearTimeout
+// - window.clearInterval
+// - window.open => opens a new window
+// - window.close => closes the current window
+// - window.moveTo => moves the window
+// - window.resizeTo => resizes the window
+// - window.focus => focuses the window
+// - window.blur => blurs the window
+// - window.print => prints the window
+
+// Navigator Object
+// - navigator.appName => gives the name of the browser
+// - navigator.appVersion => gives the version of the browser
+// - navigator.userAgent => gives the name and version of the browser
+// - navigator.platform => gives the platform of the browser
+// - navigator.language => gives the language of the browser
+// - navigator.cookieEnabled => checks if cookies are enabled in the browser
+
+navigator.clipboard.writeText("Hello, World!");
+
+// Screen Object
+// - screen.width => gives the width of the screen
+// - screen.height => gives the height of the screen
+// - screen.availWidth => gives the available width of the screen
+// - screen.availHeight => gives the available height of the screen
+
+// Location Object
+// - location.href => gives the URL of the current page
+// - location.protocol => gives the protocol of the current page
+// - location.host => gives the host of the current page
+// - location.hostname => gives the hostname of the current page
+// - location.port => gives the port of the current page
+// - location.pathname => gives the pathname of the current page
+// - location.search => gives the search of the current page
+// - location.hash => gives the hash of the current page
+
+// History Object
+// - history.back => goes back to the previous page
+// - history.forward => goes forward to the next page
+// - history.go => goes to a specific page
+// - history.length => gives the length of the history
+// - history.state => gives the state of the history
+
